3b365112d882f878b5ca116b1a91ef625e1fa1a0
JacopoBonta <bonta.jacopo@gmail.com>
2019-08-05 14:10:20 +0200

    add camera

---
+++ src/CameraCreator.lua
@@ -0,0 +1,35 @@
+function CameraCreator()
+
+    local camera = {
+        objects = display.newGroup()
+    }
+
+    function camera:addDisplayObject(o)
+        self.objects:insert(o)
+    end
+
+    function camera:setPosition(x, y)
+        self.objects.x = x
+        self.objects.y = y
+    end
+
+    function camera:moveForward(speed)
+        self.objects.x = self.objects.x - speed
+    end
+
+    function camera:moveBackward(speed)
+        self.objects.x = self.objects.x + speed
+    end
+
+    function camera:moveUp(speed)
+        self.objects.y = self.objects.y + speed
+    end
+
+    function camera:moveDown(speed)
+        self.objects.y = self.objects.y - speed
+    end
+
+    return camera
+end
+
+return CameraCreator
