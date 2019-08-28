8dbc41236e8123f6e4f06355319041300074f1a2
JacopoBonta <bonta.jacopo@gmail.com>
2019-08-28 19:08:15 +0200

    hide status bar

--- main.lua
+++ main.lua
@@ -1,6 +1,11 @@
 local composer = require( "composer" )
  
 -- Code to initialize your app
+
+-- hide status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- enable multitouch
 system.activate( "multitouch" )
  
 composer.gotoScene( "scenes.game", {
