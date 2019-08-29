27c825b5437120bc600ba53210a3e45458ef7350
JacopoBonta <bonta.jacopo@gmail.com>
2019-08-29 12:02:09 +0200

    Move default properties declaration out of the constructor into the Prototype definition

--- src/Button.lua
+++ src/Button.lua
@@ -1,26 +1,25 @@
--- Button is a base class for creating buttons object.
--- Use the ButtonText child class or ButtonImage child class in your game
-local Button = {}
+-- Button is a base class for creating buttons object. It is not intended to use as it is. Instead, use the ButtonText or ButtonImage child classes.
 
--- new() the constructor. Return a new instance of the Button class
-function Button:new(o)
-    -- define  some default properties common to all the Button class' instances
-    o = o or {
-        displayObject = nil,
-        x = display.contentWidth / 2,
-        y = display.contentHeight / 2,
-    }
-    setmetatable(o, self)
-    self.__index = self
+-- Button here is the prototype for our buttons. We provide some intial values for the button position. Every button objects will have an own copy of these properties along with the methods defined.
+local Button = {
+    x = display.contentWidth / 2,
+    y = display.contentHeight / 2,
+}
+
+-- new() method constructor. Create a Button object.
+function Button:new()
+    local o = {} -- the new object
+    setmetatable(o, self) -- here we set the Button table as the metatable of our new object 'o'
+    self.__index = self -- here we tell lua to look up at the Button table when accessing fields not present in 'o' - setting the Button table as the prototype of 'o'
     return o
 end
 
--- registerBeforeTouchHandler() method register a callback fucntion to call when the button is pressed
+-- registerBeforeTouchHandler() method register a callback fucntion that will be called when the button is pressed
 function Button:registerBeforeTouchHandler(cb)
     self.beforeCb = cb
 end
 
--- registerBeforeTouchHandler() method register a callback fucntion to call after the button is pressed
+-- registerBeforeTouchHandler() method register a callback fucntion that will be called when the button is released
 function Button:registerAfterTouchHandler(cb)
     self.afterCb = cb
 end
